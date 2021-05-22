10b3d7a813b449ee9e9f037f7ef3003db5b10765
Ellis Hughes <ehhughes@scharp.org>
2021-05-21 21:29:00 -0700

    update description

---
+++ R/openMic.R
@@ -0,0 +1,54 @@
+#' openMic
+#'
+#' openMic gives you a way to have live captioning appear over your slides!
+#'
+#' @return An `htmltools::tagList()` with the tile view dependencies, or an
+#'   [htmltools::htmlDependency()].
+#' @section Usage: To add live captioning to your xaringan presentation, add the
+#'   following code chunk to your slides' R Markdown file.
+#'
+#'   ````markdown
+#'   ```{r open-mic, echo=FALSE}
+#'   openMic::openMic()
+#'   ```
+#'   ````
+#'
+#' @name openMic
+NULL
+
+#' @describeIn openMic Adds closed captioning to your xaringan slides.
+#' @param location location where closed captions should appear: "top" or "bottom"
+#' @importFrom htmltools tagList
+#' @export
+openMic <- function(location = c("bottom","top")) {
+
+  location <- match.arg(location)
+
+  htmltools::tagList(
+    html_dependency_open_mic(location = location)
+  )
+}
+
+#' @describeIn openMic Returns an [htmltools::htmlDependency()] with the openMic
+#'   dependencies. Most users will want to use `openMic()`.
+#' @importFrom htmltools htmlDependency
+
+#' @export
+html_dependency_open_mic <- function(location) {
+
+  opts <- sprintf(
+    '<script id="openMic-cc-options" type="application/json">%s</script>',
+    paste0("{\"loc\":\"",location,"\"}")
+  )
+
+  htmltools::htmlDependency(
+    name = "openMic",
+    version = "0.0.1",
+    package = "openMic",
+    src = "openMic",
+    script = "openMic.js",
+    stylesheet = "openMic.css",
+    head = opts,
+    all_files = FALSE
+  )
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+
 # openMic
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of openMic is to add closed captions easily to your presentations using the built-in capabilities of your browser (SpeechRecognition or webkitSpeechRecognition).
+
+This was heavily influenced by the [{xaringanExtra} package by Garrick Aiden-Buie](https://github.com/gadenbuie/xaringanExtra) and a [speech-recognition example by Dave Rupert](https://codepen.io/davatron5000/pen/IKAxb). 
+
+## Installation
+
+You can install the released version of {openMic} from github with:
+
+``` r
+remotes::install_github("thebioengineer/openMic")
+```
+
+## Example
+
+This package is for creating closed captions in xaringan (or html) presentations. It floats at either the top or the bottom. This will only work for presentations opened in chrome.
+
+Add this to your presentation by adding the following chunk into your presentation:
+
+````
+```{r openMic, include = FALSE}
+openMic::openMic()
+```
+````
+
+The captioning starts by default (you may have to give permission to start recording to chrome). Move the closed captions to the top or bottom of your screen using the `u` or `d` keys on your keyboard. 
+
+To stop closed captioning, hold shift and double click the `a` button. Doing that again will restart the closed captioning.
